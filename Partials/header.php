2358fb6b3d32b955b6d807936f3efcc39cbed627
GUSTAVO BATISTA CALHERANI <25253922@educ123.sp.senai.br>
2026-05-11 10:52:11 -0300

    v1.1

---
+++ Partials/header.php
@@ -0,0 +1,8 @@
+<header>
+    <img src="Img/logo.png" class="logo"/>
+    <ul class="Header">
+        <li><a href="index.php" >Home</a></li>
+        <li></li>
+        <li><a href="AboutUs.php" >Sobre nós</a></li>
+    </ul>
+</header>
--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Soluções para Empresas | Manpower | Recrutamento & Seleção</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-    
+<?php include_once "Partials/header.php";?>
+<?php include_once "Partials/footer.php";?>
 </body>
 </html>
