4269f9e78e7ac39b9316987dfec9ffc8e0c93736
julianafreiitas <juliana.freitasmk2@gmail.com>
2026-06-10 10:54:14 -0300

    fim2

--- MySQL/crud.php
+++ MySQL/crud.php
@@ -4,7 +4,7 @@
 $host = "127.0.0.1";
 $port = 3306;
 $dbname = "Empresa";
-$username = "coiso";
+$username = "dev";
 $password = "123";
 
 try {
@@ -84,7 +84,7 @@ try {
 }
 
 $servername = "localhost";
-$username = "coiso";
+$username = "dev";
 $password = "123";
 $dbname2 = "Empresa";
 
